a486fed2254a3caf9a23b199719c17278b84bcd3
kenshin-kiba <system.labo9010nk@gmail.com>
2025-02-23 16:18:20 +0900

    kadai_012

--- kadai_010/myproject/crud/models.py
+++ kadai_010/myproject/crud/models.py
@@ -2,9 +2,18 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    img = models.ImageField(blank=True, default='noImage.png')
 
     def __str__(self):
         return self.name
--- kadai_010/myproject/myproject/urls.py
+++ kadai_010/myproject/myproject/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from crud import views
-from crud.views import ProductListView, ProductCreateView, ProductUpdateView, ProductDeleteView, ProductDetailView
+from django.conf import settings
+from django.conf.urls.static import static
+#from crud.views import ProductListView, ProductCreateView, ProductUpdateView, ProductDeleteView, ProductDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +28,8 @@ urlpatterns = [
     path('crud/new/', views.ProductCreateView.as_view(), name="new"),
     path('crud/edit/<int:pk>', views.ProductUpdateView.as_view(), name="edit"),
     path('crud/delete/<int:pk>', views.ProductDeleteView.as_view(), name="delete"),
-    path('crud/detail/<int:pk>', views.ProductDetailView.as_view(), name="detail"),
+   # path('crud/detail/<int:pk>', views.ProductDetailView.as_view(), name="detail"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
